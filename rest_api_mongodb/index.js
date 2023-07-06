e297daa28b2af18f5ac69e1b62063da5aa25e5ef
diegosanchezd <diego.sanchez.et32@gmail.com>
2023-07-06 20:38:46 -0300

    feat/rest_api finished

--- rest_api_mongodb/index.js
+++ rest_api_mongodb/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const createRouter = require('./router');
-
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.set('port', process.env.PORT || 3000);
 
 const uri = 'mongodb+srv://diegosanchezet32:diegosanchezet32@clusterdiego.dhqfrd7.mongodb.net/';
--- rest_api_mongodb/router.js
+++ rest_api_mongodb/router.js
@@ -5,6 +5,20 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
+
+const jwt = require('jsonwebtoken');
+
+function generateAuthToken(user) {
+  // Define la información que deseas incluir en el token (por ejemplo, el ID de usuario)
+  const payload = { userId: user._id };
+
+  // Genera el token utilizando la clave secreta (puede ser cualquier cadena de caracteres)
+  const token = jwt.sign(payload, '15120364', { expiresIn: '1h' });
+
+  return token;
+}
+
+
 function createRouter(db) {
     router.get('/getUsers', async (req, res) => {
         try {
@@ -56,6 +70,31 @@ function createRouter(db) {
             res.status(500).json({ error: 'Error al eliminar el usuario' });
         }
     });
+
+    router.post('/login', async (req, res) => {
+        try {
+          const userCollection = db.collection('userDetails');
+          const { userName, password } = req.body;
+          
+          // Realizar la lógica de autenticación aquí, como verificar las credenciales del usuario
+          
+          // Por ejemplo, buscar al usuario en la base de datos y verificar la contraseña
+          const user = await userCollection.findOne({ userName });
+          if (!user || user.password !== password) {
+            res.status(401).json({ error: 'Credenciales inválidas' });
+            return;
+          }
+      
+          // Si las credenciales son válidas, puedes generar un token de autenticación y devolverlo en la respuesta
+          const token = generateAuthToken(user); // función para generar el token de autenticación
+          res.json({ token });
+      
+        } catch (error) {
+          console.error('Error en el inicio de sesión', error);
+          res.status(500).json({ error: 'Error en el inicio de sesión' });
+        }
+      });
+      
 
     return router;
 }
